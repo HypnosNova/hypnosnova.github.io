3506c85ad2b253b5272698e6b815cb788e412849
HypnosNova <601670314@qq.com>
2014-12-19 11:18:29 +0800

    change

--- README.md
+++
@@ -1,3 +0,0 @@
-进入网页： http://hypnosnova.github.io
-
-three.js中文网。目前在建造中。什么时候造好还不知道。反正最终要打造成梦幻乐园。
--- js/jquery.contextmenu.js
+++ js/jquery.contextmenu.js
@@ -15,7 +15,7 @@
  *     ]
  *   });
  *
- * Icon needs to be 16x16. I recommend the Fugue icon set from: http://p.yusukekamiyamane.com/ 
+ * Icon needs to be 16x16. I recommend the Fugue icon set from: http://p.yusukekamiyamane.com/
  *
  * - Joe Walnes, 2011 http://joewalnes.com/
  *   https://github.com/joewalnes/jquery-simple-context-menu
@@ -32,80 +32,92 @@ jQuery.fn.contextPopup = function(menuData) {
 		title: '',
 		items: []
 	};
-	
+
 	// merge them
 	$.extend(settings, menuData);
 
-  // Build popup menu HTML
-  function createMenu(e) {
-    var menu = $('<ul class="' + settings.contextMenuClass + '"><div class="' + settings.gutterLineClass + '"></div></ul>')
-      .appendTo(document.body);
-    if (settings.title) {
-      $('<li class="' + settings.headerClass + '"></li>').text(settings.title).appendTo(menu);
-    }
-    settings.items.forEach(function(item) {
-      if (item) {
-        var rowCode = '<li><a href="#"><span></span></a></li>';
-        // if(item.icon)
-        //   rowCode += '<img>';
-        // rowCode +=  '<span></span></a></li>';
-        var row = $(rowCode).appendTo(menu);
-        if(item.icon){
-          var icon = $('<img>');
-          icon.attr('src', item.icon);
-          icon.insertBefore(row.find('span'));
-        }
-        row.find('span').text(item.label);
-        if (item.action) {
-          row.find('a').click(function(){ item.action(e); });
-        }
-      } else {
-        $('<li class="' + settings.seperatorClass + '"></li>').appendTo(menu);
-      }
-    });
-    menu.find('.' + settings.headerClass ).text(settings.title);
-    return menu;
-  }
+	// Build popup menu HTML
+	function createMenu(e) {
+		var menu = $('<ul class="' + settings.contextMenuClass + '"><div class="' + settings.gutterLineClass + '"></div></ul>')
+			.appendTo(document.body);
+		if (settings.title) {
+			$('<li class="' + settings.headerClass + '"></li>').text(settings.title).appendTo(menu);
+		}
+		settings.items.forEach(function(item) {
+			if (item) {
+				var rowCode = '<li><a href="#"><span></span></a></li>';
+				var row = $(rowCode).appendTo(menu);
+				if (item.icon) {
+					var icon = $('<img>');
+					icon.attr('src', item.icon);
+					icon.insertBefore(row.find('span'));
+				}
+				row.find('span').text(item.label);
+				if (item.action) {
+					row.find('a').click(function() {
+						item.action(e);
+					});
+				}
+			} else {
+				$('<li class="' + settings.seperatorClass + '"></li>').appendTo(menu);
+			}
+		});
+		menu.find('.' + settings.headerClass).text(settings.title);
+		return menu;
+	}
 
-  // On contextmenu event (right click)
-  this.bind('contextmenu', function(e) {	
-    var menu = createMenu(e)
-      .show();
-    
-    var left = e.pageX + 5, /* nudge to the right, so the pointer is covering the title */
-        top = e.pageY;
-    if (top + menu.height() >= $(window).height()) {
-        top -= menu.height();
-    }
-    if (left + menu.width() >= $(window).width()) {
-        left -= menu.width();
-    }
+	// On contextmenu event (right click)
+	this.bind('contextmenu', function(e) {
+		var menu = createMenu(e)
+			.show();
 
-    // Create and show menu
-    menu.css({zIndex:1000001, left:left, top:top})
-      .bind('contextmenu', function() { return false; });
+		var left = e.pageX + 5,
+			/* nudge to the right, so the pointer is covering the title */
+			top = e.pageY;
+		if (top + menu.height() >= $(window).height()) {
+			top -= menu.height();
+		}
+		if (left + menu.width() >= $(window).width()) {
+			left -= menu.width();
+		}
 
-    // Cover rest of page with invisible div that when clicked will cancel the popup.
-    var bg = $('<div></div>')
-      .css({left:0, top:0, width:'100%', height:'100%', position:'absolute', zIndex:1000000})
-      .appendTo(document.body)
-      .bind('contextmenu click', function() {
-        // If click or right click anywhere else on page: remove clean up.
-        bg.remove();
-        menu.remove();
-        return false;
-      });
+		// Create and show menu
+		menu.css({
+				zIndex: 1000001,
+				left: left,
+				top: top
+			})
+			.bind('contextmenu', function() {
+				return false;
+			});
 
-    // When clicking on a link in menu: clean up (in addition to handlers on link already)
-    menu.find('a').click(function() {
-      bg.remove();
-      menu.remove();
-    });
+		// Cover rest of page with invisible div that when clicked will cancel the popup.
+		var bg = $('<div></div>')
+			.css({
+				left: 0,
+				top: 0,
+				width: '100%',
+				height: '100%',
+				position: 'absolute',
+				zIndex: 1000000
+			})
+			.appendTo(document.body)
+			.bind('contextmenu click', function() {
+				// If click or right click anywhere else on page: remove clean up.
+				bg.remove();
+				menu.remove();
+				return false;
+			});
 
-    // Cancel event, so real browser popup doesn't appear.
-    return false;
-  });
+		// When clicking on a link in menu: clean up (in addition to handlers on link already)
+		menu.find('a').click(function() {
+			bg.remove();
+			menu.remove();
+		});
 
-  return this;
+		// Cancel event, so real browser popup doesn't appear.
+		return false;
+	});
+
+	return this;
 };
-
